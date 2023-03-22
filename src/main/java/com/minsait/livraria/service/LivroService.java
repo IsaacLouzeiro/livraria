2abb443c7b1cfe35cf670a761727ba27ff26783f
IsaacLouzeiro <isaacelias1110@gmail.com>
2023-03-22 07:37:01 -0300

    Inserindo o GET para Livro

--- src/main/java/com/minsait/livraria/service/LivroService.java
+++ src/main/java/com/minsait/livraria/service/LivroService.java
@@ -1,5 +1,8 @@
 package com.minsait.livraria.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +21,12 @@ public class LivroService {
 	
 	public Livro cadastrarLivro(Livro livro) {
 		return this.livroRepository.save(livro);
-
-		
+	}
+	
+	public List<Livro> exibirTodosOsLivros() {
+		List<Livro> livro = new ArrayList<Livro>();
+		livroRepository.findAll().forEach(livro1 -> livro.add(livro1));
+		return livro;
 	}
 	
 }
